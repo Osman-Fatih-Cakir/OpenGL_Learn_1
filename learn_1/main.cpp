88b8e9cba070850e122be04887c0d434641ed84f
Osman-Fatih-Cakir <cakirosmanfatih@gmail.com>
2021-02-25 17:43:23 +0300

    Transformation smoothly

--- learn_1/main.cpp
+++ learn_1/main.cpp
@@ -1,8 +1,11 @@
 
+// TODO care multiple inputs simultaneously
+
 #include <GL\glew.h>
 #include <GL\freeglut.h>
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
+#include <chrono>
 #include <iostream>
 
 #include <shaders.h>
@@ -11,6 +14,12 @@ typedef glm::mat3 mat3;
 typedef glm::mat4 mat4;
 typedef glm::vec3 vec3;
 typedef glm::vec4 vec4;
+
+//int frame = 0;
+
+// Timers
+auto old_time = std::chrono::high_resolution_clock::now();
+auto new_time = std::chrono::high_resolution_clock::now();
 
 int WIDTH = 800;
 int HEIGHT = 800;
@@ -68,16 +77,24 @@ GLushort cube_elements[] = {
 };
 GLuint cubeVAO;
 
-float angle = 15.f;
+float camera_translate_speed = 0.03f;
+float camera_rotate_angle = 5.f;
+
+
+bool is_camera_translate[6] = { false };
+bool is_camera_rotate[6] = { false };
 
 void init(void);
 void init_cube_buffers();
-void keyboard(unsigned char, int, int);
-void special_keyboard(int, int, int);
-mat4 translate(float, float, float); 
-mat4 rotateX(float);
-mat4 rotateY(float);
-mat4 rotateZ(float);
+void key_down(unsigned char, int, int);
+void key_up(unsigned char key, int x, int y);
+void special_key_down(int, int, int);
+void special_key_up(int key, int x, int y);
+mat4 translate(float x, float y, float z, std::chrono::duration<float> delta);
+mat4 rotateX(float x, std::chrono::duration<float> delta);
+mat4 rotateY(float y, std::chrono::duration<float> delta);
+mat4 rotateZ(float z, std::chrono::duration<float> delta);
+void camera_transform(std::chrono::duration<float> delta);
 void draw_cube();
 void changeViewport(int, int);
 void render();
@@ -98,8 +115,12 @@ int main(int argc, char* argv[])
 	// Bind functions
 	glutReshapeFunc(changeViewport);
 	glutDisplayFunc(render);
-	glutKeyboardFunc(keyboard);
-	glutSpecialFunc(special_keyboard);
+
+	glutKeyboardFunc(key_down);
+	glutKeyboardUpFunc(key_up);
+
+	glutSpecialFunc(special_key_down);
+	glutSpecialUpFunc(special_key_up);
 
 	// Initialize Glew
 	GLenum err = glewInit();
@@ -123,21 +144,25 @@ void changeViewport(int w, int h)
 }
 
 // Returns a homogenious translation matrix
-mat4 translate(float x, float y, float z)
+mat4 translate(float x, float y, float z, std::chrono::duration<float> delta)
 {
+	float xx = x * std::chrono::duration_cast<std::chrono::milliseconds>(delta).count() * camera_translate_speed;
+	float yy = y * std::chrono::duration_cast<std::chrono::milliseconds>(delta).count() * camera_translate_speed;
+	float zz = z * std::chrono::duration_cast<std::chrono::milliseconds>(delta).count() * camera_translate_speed;
+
 	// Translation matrix (column-majored)
 	mat4 matrix = mat4(
 		1.0f, 0.0f, 0.0f, 0.0f,
 		0.0f, 1.0f, 0.0f, 0.0f,
 		0.0f, 0.0f, 1.0f, 0.0f,
-		x, y, z, 1.0f
+		xx, yy, zz, 1.0f
 	);
 
 	return matrix;
 }
 
 // Returns a rotate matrix
-mat4 rotateX(float angle)
+mat4 rotateX(float angle, std::chrono::duration<float> delta)
 {
 	float c = glm::cos(angle);
 	float s = glm::sin(angle);
@@ -153,7 +178,7 @@ mat4 rotateX(float angle)
 }
 
 // Returns a rotate matrix
-mat4 rotateY(float angle)
+mat4 rotateY(float angle, std::chrono::duration<float> delta)
 {
 	float c = glm::cos(angle);
 	float s = glm::sin(angle);
@@ -169,7 +194,7 @@ mat4 rotateY(float angle)
 }
 
 // Returns a rotate matrix
-mat4 rotateZ(float angle)
+mat4 rotateZ(float angle, std::chrono::duration<float> delta)
 {
 	float c = glm::cos(angle);
 	float s = glm::sin(angle);
@@ -184,59 +209,110 @@ mat4 rotateZ(float angle)
 	return rz;
 }
 
-// Keyboard inputs
-void keyboard(unsigned char key, int x, int y)
+// Key press
+void key_down(unsigned char key, int x, int y)
 {
 	switch (key)
 	{
 		case 'w': // Press 'w' to move forward with camera
-			modelview *= translate(0.f, 0.0f, 0.1f);
+			is_camera_translate[0] = true;
 			break;
 		case 's': // Press 's' to move backward with camera
-			modelview *= translate(0.f, 0.0f, -0.1f);
+			is_camera_translate[1] = true;
 			break;
-		case 'a': // Press 'e' to move left with camera
-			modelview *= translate(0.1f, 0.f, 0.f);
+		case 'a': // Press 'a' to move left with camera
+			is_camera_translate[2] = true;
 			break;
 		case 'd': // Press 'd' to move right with camera
-			modelview *= translate(-0.1f, 0.f, 0.f);
+			is_camera_translate[3] = true;
 			break;
 		case 'r': // Press 'r' to move up with camera
-			modelview *= translate(0.f, -0.1f, 0.f);
+			is_camera_translate[4] = true;
 			break;
 		case 'f': // Press 'f' to move down with camera
-			modelview *= translate(0.f, 0.1f, 0.f);
+			is_camera_translate[5] = true;
 			break;
 	};
-	glutPostRedisplay();
 }
 
-// Special keyboard inputs
-void special_keyboard(int key, int x, int y)
+// Key release function
+void key_up(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+		case 'w': // Release 'w' to stop moving forward with camera
+			is_camera_translate[0] = false;
+			break;
+		case 's': // Release 's' to stop moving backward with camera
+			is_camera_translate[1] = false;
+			break;
+		case 'a': // Release 'e' to stop moving left with camera
+			is_camera_translate[2] = false;
+			break;
+		case 'd': // Release 'd' to stop moving right with camera
+			is_camera_translate[3] = false;
+			break;
+		case 'r': // Release 'r' to stop moving up with camera
+			is_camera_translate[4] = false;
+			break;
+		case 'f': // Release 'f' to stop moving down with camera
+			is_camera_translate[5] = false;
+			break;
+	};
+}
+
+// Special key press
+void special_key_down(int key, int x, int y)
 {
 	switch (key)
 	{
 		// Rotate camera
 		case GLUT_KEY_UP:
-			modelview *= rotateX(glm::radians(angle));
+			is_camera_rotate[0] = true;
 			break;
 		case GLUT_KEY_DOWN:
-			modelview *= rotateX(glm::radians(-angle));
+			is_camera_rotate[1] = true;
 			break;
 		case GLUT_KEY_LEFT:
-			modelview *= rotateY(glm::radians(angle));
+			is_camera_rotate[2] = true;
 			break;
 		case GLUT_KEY_RIGHT:
-			modelview *= rotateY(glm::radians(-angle));
+			is_camera_rotate[3] = true;
 			break;
 		case GLUT_KEY_PAGE_UP:
-			modelview *= rotateZ(glm::radians(angle));
+			is_camera_rotate[4] = true;
 			break;
 		case GLUT_KEY_PAGE_DOWN:
-			modelview *= rotateZ(glm::radians(-angle));
+			is_camera_rotate[5] = true;
 			break;
 	};
-	glutPostRedisplay();
+}
+
+// Special key release
+void special_key_up(int key, int x, int y)
+{
+	switch (key)
+	{
+		// Rotate camera
+		case GLUT_KEY_UP:
+			is_camera_rotate[0] = false;
+			break;
+		case GLUT_KEY_DOWN:
+			is_camera_rotate[1] = false;
+			break;
+		case GLUT_KEY_LEFT:
+			is_camera_rotate[2] = false;
+			break;
+		case GLUT_KEY_RIGHT:
+			is_camera_rotate[3] = false;
+			break;
+		case GLUT_KEY_PAGE_UP:
+			is_camera_rotate[4] = false;
+			break;
+		case GLUT_KEY_PAGE_DOWN:
+			is_camera_rotate[5] = false;
+			break;
+	};
 }
 
 // Initialize the program and start
@@ -264,6 +340,62 @@ void init(void)
 	init_cube_buffers();
 
 	glEnable(GL_DEPTH_TEST);
+}
+
+// Apply camera transformations
+void camera_transform(std::chrono::duration<float> delta)
+{
+	// Camera translation
+	if (is_camera_translate[0])
+	{
+		modelview *= translate(0.f, 0.0f, 0.1f, delta);
+	}
+	else if (is_camera_translate[1])
+	{
+		modelview *= translate(0.f, 0.0f, -0.1f, delta);
+	}
+	else if (is_camera_translate[2])
+	{
+		modelview *= translate(0.1f, 0.f, 0.f, delta);
+	}
+	else if (is_camera_translate[3])
+	{
+		modelview *= translate(-0.1f, 0.f, 0.f, delta);
+	}
+	else if (is_camera_translate[4])
+	{
+		modelview *= translate(0.f, -0.1f, 0.f, delta);
+	}
+	else if (is_camera_translate[5])
+	{
+		modelview *= translate(0.f, 0.1f, 0.f, delta);
+	}
+
+	// Camera rotation
+	if (is_camera_rotate[0])
+	{
+		modelview *= rotateX(glm::radians(camera_rotate_angle), delta);
+	}
+	else if (is_camera_rotate[1])
+	{
+		modelview *= rotateX(glm::radians(-camera_rotate_angle), delta);
+	}
+	else if (is_camera_rotate[2])
+	{
+		modelview *= rotateY(glm::radians(camera_rotate_angle), delta);
+	}
+	else if (is_camera_rotate[3])
+	{
+		modelview *= rotateY(glm::radians(-camera_rotate_angle), delta);
+	}
+	else if (is_camera_rotate[4])
+	{
+		modelview *= rotateZ(glm::radians(camera_rotate_angle), delta);
+	}
+	else if (is_camera_rotate[5])
+	{
+		modelview *= rotateZ(glm::radians(-camera_rotate_angle), delta);
+	}
 }
 
 // Initialize cube buffers and return
@@ -309,6 +441,13 @@ void draw_cube()
 
 void render()
 {
+	// Timer
+	new_time = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<float> delta = new_time - old_time;
+
+	// Camera transformation
+	camera_transform(delta);
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.5, 0.5, 0, 1);
 	
@@ -319,6 +458,11 @@ void render()
 	// Draw cube
 	draw_cube();
 
-
 	glutSwapBuffers();
+
+	GLuint err = glGetError(); if (err) fprintf(stderr, "%s\n", gluErrorString(err)); // Error check
+
+	old_time = new_time; // Timer
+
+	glutPostRedisplay(); // For render loop
 }
